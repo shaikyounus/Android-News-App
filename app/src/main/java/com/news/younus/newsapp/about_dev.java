2763b42d19b67e8df8798cca2d1ff1e293b5759d
SHAIK MOHAMMAD YOUNUS <shaikyounusshaik@gmail.com>
2020-10-11 19:40:15 +0530

    first commit

---
+++ app/src/main/java/com/news/younus/newsapp/about_dev.java
@@ -0,0 +1,20 @@
+package com.news.younus.newsapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+public class about_dev extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_dev);
+
+
+    }
+}
